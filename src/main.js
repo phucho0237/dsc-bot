dc03322d5f74de7df85edf8fa7df075270d7837c
Nhu Phuc <phucho0237@gmail.com>
2023-11-05 17:13:22 +0700

    main file

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-const { Client } = require("discord.js");
+const { Client, GatewayIntentBits, Events } = require("discord.js");
 
-const client = new Client();
+const config = require("./config");
+
+const client = new Client({
+   intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages
+   ]
+});
+
+client.once(Events.ClientReady, c => {
+   console.log(`[CLIENT] - Logged in as ${c.user.tag} (ID: ${c.user.id})`);
+});
+
+client.login(config.bot.token);
