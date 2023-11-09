50407e23c0c09d0efcd4bf92e737fde15579041c
Nhu Phuc <phucho0237@gmail.com>
2023-11-09 21:36:51 +0700

    update some event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,15 @@
-const { Events } = require("discord.js");
+const { Events, Client } = require("discord.js");
 
 module.exports = {
    name: Events.ClientReady,
    once: true,
+   /**
+    *
+    * @param {Client} client
+    */
    execute(client) {
-      console.log(`Ready! Logged in as ${client.user.tag}`);
+      console.log(
+         `[CLIENT] - Logged in as ${client.user.tag} (ID: ${client.user.id})`
+      );
    }
 };
--- src/events/interaction/interactionCreate.js
+++ src/events/interaction/interactionCreate.js
@@ -1,7 +1,11 @@
-const { Events } = require("discord.js");
+const { Events, ChatInputCommandInteraction } = require("discord.js");
 
 module.exports = {
    name: Events.InteractionCreate,
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    */
    async execute(interaction) {
       if (!interaction.isChatInputCommand()) return;
 
