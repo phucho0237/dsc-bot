cd6b0d55fa1e2392585538317ad829d935ef2dd3
Nhu Phuc <phucho0237@gmail.com>
2024-01-01 08:37:35 +0000

    check if user provide the connection string or not

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -15,6 +15,11 @@ module.exports = {
          `[CLIENT] - Logged in as ${client.user.tag} (ID: ${client.user.id})`
       );
 
+      if (!config.db.url)
+         return console.log(
+            "[MONGODB] - No connection string provided, some bot features will not work"
+         );
+
       mongoose
          .connect(config.db.url)
          .then(() => console.log("[MONGODB] - Connected to database"))
